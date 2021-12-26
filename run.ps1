ce4bbce91ab335adec75753571e3d41df68dc87a
Sean <instantiateseankim@gmail.com>
2021-12-26 18:24:39 -0500

    removed restart script

--- run.ps1
+++ run.ps1
@@ -38,52 +38,5 @@ $args+=$mods
 
 $server = Start-Process -FilePath $CMD -ArgumentList $args -passthru
 
-#Restart Sript
-
-$H = Get-Host
-$Win = $H.UI.RawUI.WindowSize
-$Win.Height = 10
-$Win.Width  = 65
-$H.UI.RawUI.Set_WindowSize($Win)
-#Title of the Powershell/CMD Prompt window
-$H.ui.RawUI.WindowTitle = ''
-
 #Set Processor Affinity
 #$server.ProcessorAffinity=3840
-
-#When to Restart in HHmmss
-$restartTime = 070000
-$restartCounter = 0
-#If the console window is closed also close the server
-Register-EngineEvent PowerShell.Exiting {Stop-Process -id $server.Id}
-
-#Console loop and server restart loop
-while ($true) {
-    #Get time and convert into Int
-    $time = [convert]::ToInt32((Get-Date -Format HHmmss), 10)
-    #If between restartTime and restartTime + 10 shut down the server and start after 3 seconds
-    if (($time -ge $restartTime) -and ($time -le ($restartTime + 2))) {
-        Stop-Process -id $server.Id
-        $restartCounter++
-        Start-Sleep -Seconds 3
-        $server = Start-Process -FilePath $CMD -ArgumentList $args -passthru
-		$server.ProcessorAffinity=3840
-    }
-    #If process is not found shut down the script udpate the window
-    if (Get-Process -Id $server.Id){
-		Clear-Host
-		#Put the name of the server below
-		""
-        Get-Process -Id $server.Id
-        "Amount of times restarted:"+$restartCounter
-    } else {
-		Clear-Host
-		""
-		""
-		"Server Not Found"
-        "Shutting Down."
-		Start-Sleep -Seconds 3
-        Break
-    }
-    Start-Sleep -Seconds 1
-}
