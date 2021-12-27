fc049e1ecae2d7fdab10db755dad59d17d492230
Sean <instantiateseankim@gmail.com>
2021-12-27 01:59:08 -0500

    changed mod selection to array

--- run.ps1
+++ run.ps1
@@ -13,33 +13,50 @@ $args=''
 $modDir=''
 
 #Add mods from Worksop folder to the server startup
-$mods=' "-mod=argo;curator;heli;jets;kart;mark;orange;'
-$mods+= $modDir + '' + ';'
-$mods+= '"'
+$modsArray = @(
+	''
+)
 
 #Add mods from Worksop folder to the server startup
 #$modDir='C:\Program Files (x86)\Steam\steamapps\common\Arma 3\!Workshop\'
 
-$mods+= ' "-serverMod='
-$mods+= $modDir + '' + ';'
-$mods+= '"'
+$serverModArray = @(
+    ''
+)
+
+#You don't have to edit beyond this point
 
 #Details on this folder can be found here:
 #https://community.bistudio.com/wiki/server.cfg
-$profiles='"-config=' + $profileDir + '/config/server.cfg"'
+$profiles='"-config=' + $profileDir + '\config\server.cfg"'
 
 #Details on this folder can be found here:
 #https://community.bistudio.com/wiki/basic.cfg
-$profiles+='"-cfg=' + $profileDir + '/config/basic.cfg"'
+$profiles+='"-cfg=' + $profileDir + '\config\basic.cfg"'
 
 #Setting the directory of server profile data.
 #This is the directory that contains all the server logs
 $profiles+='"-profiles=' + $profileDir + '"'
 
 $args=$profiles + $args
-$args+=$mods
 
-$server = Start-Process -FilePath $CMD -ArgumentList $args -passthru
+if (($modsArray.length -gt 0) -and ($modsArray[0].length -gt 0)){
+    $mods=' -mod=curator;kart;heli;mark;expansion;jets;argo;orange;tacops;tank;enoch;aow;'
+    for($i = 0; $i -lt $modsArray.length; $i++){
+	    $mods += $modDir + '\' + $modsArray[$i] + ';'
+    }
+    $args+=$mods
+}
+
+if (($serverModArray.length -gt 0) -and ($serverModArray[0].length -gt 0)){
+    $serMods+= ' -serverMod='
+    for($i = 0; $i -lt $serverModArray.length; $i++){
+	    $serMod+= $modDir + '\' + $serverModArray[$i] + ';'
+    }
+    $args+=$serMods
+}
+
+#$server = Start-Process -FilePath $CMD -ArgumentList $args -passthru
 
 #Set Processor Affinity
 #$server.ProcessorAffinity=3840
