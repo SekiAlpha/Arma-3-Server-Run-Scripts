819350ca7c99571a630080879599d0c9539674f0
Sean <instantiateseankim@gmail.com>
2021-12-28 01:38:49 -0500

    Added detection of directories and files
    User will now be prompted in a few edge cases that will cause the script to not work as intended.

--- run.ps1
+++ run.ps1
@@ -28,17 +28,38 @@ $serverModArray = @(
 
 #Details on this folder can be found here:
 #https://community.bistudio.com/wiki/server.cfg
-$profiles='"-config=' + $profileDir + '\config\server.cfg"'
+$serverCfg='"-config=' + $profileDir + '\config\server.cfg"'
 
 #Details on this folder can be found here:
 #https://community.bistudio.com/wiki/basic.cfg
-$profiles+='"-cfg=' + $profileDir + '\config\basic.cfg"'
+$basicCfg+='"-cfg=' + $profileDir + '\config\basic.cfg"'
 
 #Setting the directory of server profile data.
 #This is the directory that contains all the server logs
 $profiles+='"-profiles=' + $profileDir + '"'
 
-$args=$profiles + $args
+$args=$profiles + $serverCfg + $basicCfg
+
+#Detection if all required directories exist
+
+if (($CMD -eq '') -or ($profileDir -eq  '') -or ($modDir -eq '')){
+    Read-Host "Looks like some variables have been left empty. Press enter to close the program"
+    Exit
+}
+
+if (-not (Test-Path $CMD -PathType leaf)){
+    Read-Host "Looks like the location of the arma 3 exe was not found. Press enter to close the program"
+    Exit
+}
+if (-not (Test-Path $profileDir -PathType container)){
+    Read-Host "Looks like the profileDir value you entered isn't a folder. Press enter to close the program"
+    Exit
+}
+if (-not (Test-Path $modDir -PathType container)){
+    Read-Host "Looks like the modDir value you entered isn't a folder. Press enter to close the program"
+    Exit
+}
+
 
 if (($modsArray.length -gt 0) -and ($modsArray[0].length -gt 0)){
     $mods=' -mod=curator;kart;heli;mark;expansion;jets;argo;orange;tacops;tank;enoch;aow;'
